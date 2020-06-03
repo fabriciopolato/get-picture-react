d5aa891f056316acf9565550e8e7994a60228a25
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-06-03 12:43:31 -0300

    update image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Image = ({ className, img }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  console.log(isHovered);
+
+  const handleHoverEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleHoverLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} className="image-grid" alt="some pic" />
+      <img
+        onMouseEnter={handleHoverEnter}
+        onMouseLeave={handleHoverLeave}
+        src={img.url}
+        className="image-grid"
+        alt="some pic"
+      />
     </div>
   );
 };
